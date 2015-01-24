f9996d19b477d727a96d3bd3a6eac017a16894a2
Ciro Durán <ciro.duran@gmail.com>
2015-01-24 18:23:32 +0000

    Ball can submerge in water and jump

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,12 @@
 package;
 
+import flixel.addons.nape.FlxNapeState;
+import flixel.addons.nape.FlxNapeSprite;
+import nape.dynamics.InteractionFilter;
+import nape.geom.Vec2;
+import nape.phys.Body;
+import nape.phys.BodyType;
+import nape.shape.Polygon;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxObject;
@@ -8,12 +15,14 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 import flixel.util.FlxColor;
+import nape.phys.FluidProperties;
+import nape.shape.Polygon;
 import proto.BallChar;
 
 /**
  * A FlxState which can be used for the actual gameplay.
  */
-class PlayState extends FlxState
+class PlayState extends FlxNapeState
 {
 	public static var player1_key_mapping : Map<Array<String>, Int> = [
 		["W"] => FlxObject.UP,
@@ -23,7 +32,7 @@ class PlayState extends FlxState
 	];
 	
 	var ball0 : BallChar;
-	var sea : FlxSprite;
+	var sea : FlxNapeSprite;
 	var background : FlxSprite;
 	
 	var sea_line : Int = 300;
@@ -35,18 +44,38 @@ class PlayState extends FlxState
 	{
 		super.create();
 		
+		napeDebugEnabled = true;
+		
+		createWalls(0, 0, FlxG.width, FlxG.height);
+		
 		background = new FlxSprite(0, 0);
 		background.makeGraphic(800, 600, FlxColor.AQUAMARINE);
-		
-		sea = new FlxSprite(0, sea_line);
-		sea.makeGraphic(800, 300, FlxColor.NAVY_BLUE);
-		
-		ball0 = new BallChar(20, sea_line);
-		
-		
 		add(background);
+		
+		var sea_interaction_filters : InteractionFilter = new InteractionFilter();
+		sea_interaction_filters.collisionMask = 0;
+		sea_interaction_filters.fluidMask = 2;
+		var sea_fluid_properties : FluidProperties = new FluidProperties(3, 6);
+		var sea_shape : Polygon = new Polygon(Polygon.rect(-400, -150, 800, 300));
+		sea_shape.fluidEnabled = true;
+		sea_shape.filter = sea_interaction_filters;
+		sea_shape.fluidProperties.density = 2;
+		sea_shape.fluidProperties.viscosity = 3;
+		
+		var sea_body : Body = new Body(BodyType.STATIC, Vec2.weak(0, 0));
+		sea_shape.body = sea_body;
+		
+		
+		sea = new FlxNapeSprite(400, FlxG.height - sea_line/2.0 , null, false);
+		sea.makeGraphic(800, 300, FlxColor.NAVY_BLUE);
+		sea.addPremadeBody(sea_body);
+		sea.physicsEnabled = true;
 		add(sea);
+		
+		ball0 = new BallChar(300, sea_line - 100);
 		add(ball0);
+		
+		FlxNapeState.space.gravity.setxy(0, 500);
 	}
 	
 	/**
@@ -65,11 +94,18 @@ class PlayState extends FlxState
 	{
 		super.update();
 		
+		if (FlxG.keys.justPressed.G)
+		{
+			napeDebugEnabled = !napeDebugEnabled;
+		}
+		
+		ball0.is_in_water = sea.body.contains(ball0.body.position);
+		
 		var movement : Int = 0;
 		
 		for (k in player1_key_mapping.keys())
 		{
-			if (FlxG.keys.anyJustPressed(k))
+			if (FlxG.keys.anyPressed(k))
 			{
 				movement |= player1_key_mapping.get(k);
 			}
--- source/proto/BallChar.hx
+++ source/proto/BallChar.hx
@@ -1,21 +1,43 @@
 package proto;
 
-import flixel.FlxSprite;
+import flixel.addons.nape.FlxNapeSprite;
+import flixel.addons.nape.FlxNapeState;
+import nape.dynamics.InteractionFilter;
+import nape.phys.BodyType;
+import nape.geom.Vec2;
 import flixel.FlxObject;
+import flixel.FlxG;
 import flixel.util.FlxColor;
 
 /**
  * ...
  * @author Ciro Duran
  */
-class BallChar extends FlxSprite
+class BallChar extends FlxNapeSprite
 {
-
+	public var accel (default, null) : Vec2 = new Vec2(300, 1000);
+	public var is_in_water : Bool = false;
+	
 	public function new(X:Float=0, Y:Float=0, ?SimpleGraphic:Dynamic) 
 	{
-		super(X, Y, SimpleGraphic);
+		super(X, Y, null, false, false);
+		
+		createCircularBody(20, BodyType.DYNAMIC);
 		
 		makeGraphic(40, 40, FlxColor.MAUVE);
+		setBodyMaterial(0.5, 0.2, 0.5, 1, 1);
+		
+		var interactionFilter : InteractionFilter = new InteractionFilter();
+		interactionFilter.collisionGroup = 2;
+		interactionFilter.fluidGroup = 2;
+		body.setShapeFilters(interactionFilter);
+		body.position.y = Y;
+		body.position.x = X;
+		
+		physicsEnabled = true;
+		
+		FlxG.watch.add(this, "is_in_water");
+		FlxG.watch.add(this.body, "force");
 	}
 	
 	public override function update()
@@ -25,27 +47,34 @@ class BallChar extends FlxSprite
 	
 	public function move(movement : Int) : Void
 	{
-		if (movement == 0)
-		{
-			return;
-		}
+		var impulse:Vec2 = Vec2.weak();
 		
 		if (movement & FlxObject.LEFT != 0)
 		{
-			x -= 1;
+			impulse.x = -accel.x;
 		}
 		else if (movement & FlxObject.RIGHT != 0)
 		{
-			x += 1;
+			impulse.x = accel.x;
+		}
+		else
+		{
+			impulse.x = 0;
 		}
 		
-		if (movement & FlxObject.UP != 0)
+		if (movement & FlxObject.UP != 0 && is_in_water)
 		{
-			y -= 1;
+			impulse.y = -accel.y;
 		}
-		else if (movement & FlxObject.DOWN != 0)
+		else if (movement & FlxObject.DOWN != 0 && is_in_water)
 		{
-			y += 1;
+			impulse.y = accel.y;
 		}
+		else
+		{
+			impulse.y = 0;
+		}
+		
+		body.force = impulse;
 	}
 }
